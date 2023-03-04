41f5d2d0381a3c71c4525fa17f2b5edbf87b499f
erambeau <rambeaue@gmail.com>
2023-03-04 15:16:41 +0000

    build: initialize the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import React from "react"
+
 function App() {
 
   return (
     <div className="App">
-      Hello world
+      <h1>How fast do you type ?</h1>
+      <textarea />
+      <h4>Remaining time : XX</h4>
+      <button>Start</button>
+      <h1>Word Count : XX</h1>
     </div>
   )
 }
