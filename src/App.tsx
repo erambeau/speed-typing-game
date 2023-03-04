0501560cebd6e60046a67534b0b43f56d1e4dc82
erambeau <rambeaue@gmail.com>
2023-03-04 16:03:53 +0000

    feat: countdown timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, {useState, ChangeEvent} from "react"
+import React, {useState, ChangeEvent, useEffect} from "react"
 
 function App() {
 
   const [text, setText] = useState("")
+  const [timer, setTimer] = useState(10)
 
   function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
     const {value} = event.target
@@ -14,6 +15,12 @@ function App() {
     const filteredWords = wordsArr.filter(word => word !== "")
     return filteredWords.length
   }
+  
+  useEffect(() => {
+    if(timer > 0) {
+      setTimeout(() => {setTimer(prevTimer => prevTimer-1)}, 1000)
+    }
+  }, [timer])
 
   return (
     <div className="App">
@@ -22,7 +29,7 @@ function App() {
         onChange={handleChange}
         value={text}
       />
-      <h4>Remaining time : XX</h4>
+      <h4>Remaining time : {timer}</h4>
       <button onClick={() => console.log(computeCountWords(text))}>Start</button>
       <h1>Word Count : XX</h1>
     </div>
