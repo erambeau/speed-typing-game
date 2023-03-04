b4c011c0010746fc7ae7873f7d5c9712bf8bdbee
erambeau <rambeaue@gmail.com>
2023-03-04 15:27:10 +0000

    build: bind content of textarea to a state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from "react"
+import React, {useState, ChangeEvent} from "react"
 
 function App() {
+
+  const [text, setText] = useState("")
+
+  function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    const {value} = event.target
+    setText(value)
+  }
+
+  console.log(text)
 
   return (
     <div className="App">
       <h1>How fast do you type ?</h1>
-      <textarea />
+      <textarea 
+        onChange={handleChange}
+        value={text}
+      />
       <h4>Remaining time : XX</h4>
       <button>Start</button>
       <h1>Word Count : XX</h1>
