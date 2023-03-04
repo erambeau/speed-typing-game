a1ee447622c60b660859e47986a5aa2ec8145e7b
erambeau <rambeaue@gmail.com>
2023-03-04 15:47:46 +0000

    feat: add a computeCountWords function

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,11 @@ function App() {
     setText(value)
   }
 
-  console.log(text)
+  function computeCountWords(inputText: string) {
+    const wordsArr = inputText.trim().split(" ")
+    const filteredWords = wordsArr.filter(word => word !== "")
+    return filteredWords.length
+  }
 
   return (
     <div className="App">
@@ -19,7 +23,7 @@ function App() {
         value={text}
       />
       <h4>Remaining time : XX</h4>
-      <button>Start</button>
+      <button onClick={() => console.log(computeCountWords(text))}>Start</button>
       <h1>Word Count : XX</h1>
     </div>
   )
